34bc5650d7d03a1b83a905fcc77202d80e6cc6dc
eunju.kim <sb02348@hanmail.net>
2022-08-29 02:28:23 +0900

    Change Image path

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
-//import headerImg from "../assets/img/"
+import headerImg from "../assets/img/header-img.svg";
 
 
 export const Banner = () => {
@@ -10,17 +10,41 @@ export const Banner = () => {
     const toRotate = ["Web Developer", "Front-end Developer"];
     const [text, setText] = useState("");
     const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const [index, setIndex] = useState(1);
     const period = 2000;
 
     useEffect(() => {
         let ticker = setInterval(() => {
             tick();
-        }, delta)
+        }, delta);
 
         return () => { clearInterval(ticker) };
     }, [text])
 
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
 
+        setText(updatedText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta / 2);
+        }
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setIndex(prevIndex => prevIndex - 1);
+            setDelta(period);
+        } else if (isDeleting && updatedText === "") {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setIndex(1);
+            setDelta(500);
+        } else {
+            setIndex(prevIndex => prevIndex + 1);
+        }
+    }
 
     return (
         <section className='banner' id="home">
@@ -33,7 +57,7 @@ export const Banner = () => {
                         <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25} /></button>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
-                        {/* <img src={ } alt="Header Img" /> */}
+                        <img src={headerImg} alt="Header Img" />
                     </Col>
                 </Row>
             </Container>
