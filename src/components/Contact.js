f11e38139321b1f75eff850c448b6f086fed0819
eunju.kim <sb02348@hanmail.net>
2022-09-04 23:58:42 +0900

    Add Contact Component

---
+++ src/components/Contact.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+export const Contact = () => {
+
+
+    return (
+        <section className='contact' id="connect">
+            <Container>
+                <Row className="align-items-center">
+                    <Col md={6}>
+                    </Col>
+                </Row>
+            </Container>
+
+        </section>
+    )
+}
