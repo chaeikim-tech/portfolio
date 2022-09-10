0fe68ae63fbf03565359f29e56b34e264604e97a
eunju.kim <sb02348@hanmail.net>
2022-09-11 02:33:59 +0900

    Add Contact css

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -32,28 +32,28 @@ export const Contact = () => {
         <section className='contact' id="connect">
             <Container>
                 <Row className="align-items-center">
-                    <Col md={6} >
+                    <Col size={12} md={6} >
                         <img src={contactImg} alt="Contact Us" />
                     </Col>
                     <Col md={6} >
                         <h2>Get In Touch</h2>
                         <form onSubmit={handleSubmit}>
                             <Row>
-                                <Col sm={6} className="px-1">
+                                <Col size={12} sm={6} className="px-1">
                                     <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate("firstName", e.target.value)} ></input>
                                 </Col>
-                                <Col sm={6} className="px-1">
+                                <Col size={12} sm={6} className="px-1">
                                     <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e) => onFormUpdate("lastName", e.target.value)} ></input>
                                 </Col>
-                                <Col sm={6} className="px-1">
+                                <Col size={12} sm={6} className="px-1">
                                     <input type="text" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate("email", e.target.value)} ></input>
                                 </Col>
-                                <Col sm={6} className="px-1">
+                                <Col size={12} sm={6} className="px-1">
                                     <input type="text" value={formDetails.phone} placeholder="Phone Number" onChange={(e) => onFormUpdate("phone", e.target.value)} ></input>
                                 </Col>
-                                <Col>
+                                <Col size={12} className="px-1">
                                     <textarea row="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate("message", e.target.value)}></textarea>
-                                    <button type="submit"><span>{buttonText}</span></button>
+                                    <button type="submit"><span>submit</span></button>
                                 </Col>
 
                             </Row>
